6d0b42c1d1319bd6291f36e96285afdd421de71c
Cohl Dickerson <cohlldickerson@icloud.com>
2024-08-04 08:22:27 -0400

    Created Myles's website

---
+++ script.js
@@ -0,0 +1,13 @@
+function united_states(){
+    document.location.href = "../Countries/united_states.html"
+}
+
+
+function earth(){
+    document.location.href="../earth.html"
+}
+
+
+function index(){
+    document.location.href="./index.html"
+}
